71ce9b959c3a4c8132de3f9381abc9a25fe145f4
Anthony Beckford <abeckford532@gmail.com>
2022-04-08 08:15:11 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anthony Beckford on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
